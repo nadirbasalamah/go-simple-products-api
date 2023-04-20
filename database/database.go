f7ce773196b3fa261df6bc6d0abb526dd5fba911
nadirbasalamah <nadir008basalamah@gmail.com>
2023-04-20 08:33:03 +0700

    prepare for deployment

--- database/database.go
+++ database/database.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"go-simple-products-api/models"
 	"log"
+	"os"
 
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
@@ -15,11 +16,11 @@ func InitDB() {
 	var err error
 
 	var dsn string = fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True",
-		"root",
-		"",
-		"localhost",
-		"3306",
-		"echo_sample",
+		os.Getenv("DB_USERNAME"),
+		os.Getenv("DB_PASSWORD"),
+		os.Getenv("DB_HOST"),
+		os.Getenv("DB_PORT"),
+		os.Getenv("DB_NAME"),
 	)
 
 	DB, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
